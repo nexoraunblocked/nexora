6377f7f9a3434e90b70fb574e6d171a81aefa0c3
Oscar Su <nexoraunblocked@gmail.com>
2025-08-30 11:35:41 -0500

    [Feature] Website History will only show once

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+const app = document.getElementById('app');
 
+const routes = {
+  '/home': renderHome,
+  '/games': renderGames,
+  '/movies': renderMovies,
+  '/proxy': renderProxy,
+  '/hacks': renderHacks
+};
+
+function navigate(path) {
+  history.replaceState({}, '', path);
+  routes[path]?.();
+}
+
+document.querySelectorAll('.sidebar a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    navigate(link.dataset.route);
+  });
+});
+
+window.onpopstate = () => navigate('/home');
+
+if (!routes[location.pathname]) {
+  navigate('/home');
+} else {
+  navigate(location.pathname);
+}
--- views.js
+++ views.js
@@ -1,1 +1,16 @@
+function loadView(file) {
+  fetch(file)
+    .then(res => res.text())
+    .then(html => {
+      app.innerHTML = html;
+    })
+    .catch(() => {
+      app.innerHTML = `<h1 class="site-title">Error</h1><p>Failed to load ${file}.</p>`;
+    });
+}
 
+function renderHome()   { loadView('home.html'); }
+function renderGames()  { loadView('games.html'); }
+function renderMovies() { loadView('movies.html'); }
+function renderProxy()  { loadView('proxy.html'); }
+function renderHacks()  { loadView('hacks.html'); }
